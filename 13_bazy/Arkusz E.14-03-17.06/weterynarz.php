9cbe64e7cd3afeb1b533cf8b604cb984336ac38a
eryk-lawniczak <35136216+eryk-lawniczak@users.noreply.github.com>
2018-10-24 15:43:58 +0200

    lekcja 14

---
+++ 13_bazy/Arkusz E.14-03-17.06/weterynarz.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pl" >
+  <head>
+    <meta charset="utf-8">
+    <title>Weterynarz</title>
+    <link rel="stylesheet" href="weterynarz.css">
+  </head>
+  <body>
+        <div id="baner">
+          <h1>GABINET WETERYNARYJNY</h1>
+        </div>
+        <div id="lewy">
+          <h2>PSY</h2>
+          <!-- skrypt 1 -->
+          <h2>KOTY</h2>
+          <!-- skrypt 2 -->
+
+        </div>
+        <div id="srodek">
+          <h2>SZCZEGÓŁOWA INFORMACJA O ZWIERZĘTACH</h2>
+          <!-- skrypt 3 -->
+        </div>
+        <div id="prawy">
+          <h2>WETERYNARZ</h2>
+          <a href="./logo.jpg"><img src="./logo-mini.jpg" ></a>
+          <p>Krzysztof Nowakowski, lekarz weterynarii</p>
+          <h2>GODZINY PRZYJĘĆ</h2>
+          <table>
+            <tr>
+              <td>poniedziałek</td>
+              <td>15:00-19:00</td>
+            </tr>
+            <tr>
+              <td>wtorek</td>
+              <td>15:00-19:00</td>
+            </tr>
+          </table>
+        </div>
+  </body>
+</html>
