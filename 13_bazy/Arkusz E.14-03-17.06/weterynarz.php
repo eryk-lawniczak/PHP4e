df2b10601a74cf28d2e735baa821335be283e2da
eryk-lawniczak <35136216+eryk-lawniczak@users.noreply.github.com>
2018-10-30 08:55:37 +0100

    Koniec

--- 13_bazy/Arkusz E.14-03-17.06/weterynarz.php
+++ 13_bazy/Arkusz E.14-03-17.06/weterynarz.php
@@ -12,13 +12,78 @@
         <div id="lewy">
           <h2>PSY</h2>
           <!-- skrypt 1 -->
+          <?php
+            //połączenie z serwerem mysql)
+          if ($polaczenie = mysqli_connect('localhost', 'root', '')) {
+            //echo "Prawidłowe połączenie z serwerem Mysql";
+          }else{
+            echo "błędne połączenie";
+          }
+
+          //połączenie z bazą danych weterynarz
+          if(mysqli_select_db($polaczenie, 'weterynarz')){
+            //echo "Dobrze!";
+          }
+          mysqli_set_charset($polaczenie, 'utf8');
+          $zapytanie = "SELECT `id`,`imie`,`wlasciciel` FROM `zwierzeta` WHERE `rodzaj` = 1";
+echo "<table>";
+          if ($rezultat = mysqli_query($polaczenie, $zapytanie)) {
+            while ($wiersz = mysqli_fetch_row($rezultat)) {
+
+              echo <<<WIERSZ
+              <tr>
+              <td>$wiersz[0]</td>
+              <td>$wiersz[1]</td>
+              <td>$wiersz[2]</td>
+              </tr>
+WIERSZ;
+
+            }
+          }else {
+            echo "błąd";
+          }
+          echo "</table>";
+           ?>
           <h2>KOTY</h2>
           <!-- skrypt 2 -->
+          <?php
+          $zapytanie = "SELECT `id`,`imie`,`wlasciciel` FROM `zwierzeta` WHERE `rodzaj` = 2";
 
+          if ($rezultat = mysqli_query($polaczenie, $zapytanie)) {
+            while ($wiersz = mysqli_fetch_assoc($rezultat)) {
+              echo <<<WIERSZ
+              $wiersz[id]
+              $wiersz[imie]
+              $wiersz[wlasciciel]
+              <br>
+
+WIERSZ;
+            }
+          }else {
+            echo "błąd";
+          }
+           ?>
         </div>
         <div id="srodek">
           <h2>SZCZEGÓŁOWA INFORMACJA O ZWIERZĘTACH</h2>
           <!-- skrypt 3 -->
+          <?php
+          $zapytanie = "SELECT `imie`,`telefon`,`szczepienie`,`opis` FROM `zwierzeta`";
+          if ($rezultat = mysqli_query($polaczenie, $zapytanie)) {
+            while ($wiersz = mysqli_fetch_assoc($rezultat)) {
+              echo <<<WIERSZ
+              Pacjent: $wiersz[imie] <br>
+              Telefon: $wiersz[telefon],
+              Ostatnie szczepienie: $wiersz[szczepienie],
+              Informacje: $wiersz[opis]
+              <hr>
+
+WIERSZ;
+          }
+          }else {
+            echo "błąd";}
+            mysqli_close($polaczenie);
+           ?>
         </div>
         <div id="prawy">
           <h2>WETERYNARZ</h2>
