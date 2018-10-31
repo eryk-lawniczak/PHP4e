591dba77f94398d431c81398e1a0c9a28ffc93f5
eryk-lawniczak <35136216+eryk-lawniczak@users.noreply.github.com>
2018-10-31 14:13:02 +0100

    f

---
+++ 13_bazy/Arkusz E.14-01-17.06/pacjent.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pl">
+  <head>
+    <meta charset="utf-8">
+    <title>Przychodnia</title>
+    <link rel="stylesheet" href="./przychodnia.css">
+  </head>
+  <body>
+
+  <div id="baner">
+    <h1>PRAKTYKA LEKARZA RODZINNEGO</h1>
+  </div>
+  <div id="lewy">
+    <h3>LISTA PACJENTÓW</h3>
+    <!-- skrypt 1 -->
+    <br><br>
+    <form id="formularz" method="post">
+      <input type="text" name="id"><br>
+      <input type="number" name="numer"><br>
+      <input type="submit" name="przycisk" value="Pokaż dane">
+
+    </form>
+
+    <h3>LEKARZE</h3>
+    <ul>
+      <li>pn - śr
+        <ol>
+          <li>Anna Kwiatkowska</li>
+          <li>Jan Kowalewski</li>
+        </ol>
+      </li>
+      <li>czw - pt
+        <ol>
+          <li>Krzysztof Nowak</li>
+        </ol>
+      </li>
+    </ul>
+  </div>
+  <div id="prawy">
+    <h2>INFORMACJE SZCZEGÓŁOWE O PACJENCIE</h2>
+    <!-- skrypt 2-->
+    
+  </div>
+  <div id="stopka">
+    <p>utworzeone przez: PESEL</p><br>
+    <a href="./kwerendy.txt">Pobierz blik z kwerendami</a>
+  </div>
+
+  </body>
+</html>
