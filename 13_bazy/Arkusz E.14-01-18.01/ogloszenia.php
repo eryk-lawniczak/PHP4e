f547e0d149acda08b9c911c51a2f3a9f3ab31d17
eryk-lawniczak <35136216+eryk-lawniczak@users.noreply.github.com>
2018-10-31 15:34:48 +0100

    Przed obiektówką

---
+++ 13_bazy/Arkusz E.14-01-18.01/ogloszenia.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="pl" >
+  <head>
+    <meta charset="utf-8">
+    <title>Portal ogłoszeniowy</title>
+    <link rel="stylesheet" href="./styl1.css">
+  </head>
+  <body>
+    <div class="baner">
+      <h1>Portal Ogłoszeniowy</h1>
+    </div>
+    <div class="lewy">
+      <h2>Kategorie ogłoszeń</h2>
+      <ol>
+        <li>Książki</li>
+        <li>Muzyka</li>
+        <li>Filmy</li>
+      </ol>
+      <img src="./ksiazki.jpg" alt="Kupię/ Sprzedam książkę">
+      <table>
+        <tr>
+          <td>Liczba ogłoszeń</td>
+          <td>Cena ogłoszenia</td>
+          <td>Bonus</td>
+        </tr>
+        <tr>
+          <td>1 - 10</td>
+          <td>1 PLN</td>
+          <td rowspan="3"> Subskrybcja newslettera<br>
+            to upust 0,20 PLN na <br>ogłoszenie</td>
+        </tr>
+        <tr>
+          <td>11-50</td>
+          <td>0,80 PLN</td>
+
+        </tr>
+        <tr>
+          <td>51 i więcej</td>
+          <td>0,60 PLN</td>
+
+        </tr>
+      </table>
+    </div>
+    <div class="prawy">
+      <h2>Ogłoszenia kategorii książki</h2>
+      <!-- skrypt 1 -->
+    </div>
+    <div class="stopka">
+      Portal ogłoszeniowy opracował: PESEL
+    </div>
+  </body>
+</html>
